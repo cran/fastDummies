b5f226d1b96e93ec3e9d5bf9484b55887f9730a5
Jacob Kaplan <jkkaplan6@gmail.com>
2018-03-03 21:24:29 +0000

    version 1.1.0

--- R/dummy_cols.R
+++ R/dummy_cols.R
@@ -8,7 +8,7 @@
 #' @family dummy functions
 #' @seealso \code{\link{dummy_rows}} For creating dummy rows
 #'
-#' @param data
+#' @param .data
 #' An object with the data set you want to make dummy columns from.
 #' @param select_columns
 #' Vector of column names that you want to create dummy variables from.
@@ -17,8 +17,9 @@
 #' Removes the first dummy of every variable that only n-1 Dummies remain.
 #' This avoids multicollinearity issues in models.
 #' @return
-#' A data.frame with same number of rows as inputted data and original
-#' columns plus the newly created dummy columns.
+#' A data.frame (or tibble or data.table, depending on input data type) with
+#' same number of rows as inputted data and original columns plus the newly
+#' created dummy columns.
 #' @export
 #' @examples
 #' crime <- data.frame(city = c("SF", "SF", "NYC"),
@@ -30,7 +31,7 @@
 #' # Remove first dummy for each pair of dummy columns made
 #' dummy_cols(crime, select_columns = c("city", "year"),
 #'     remove_first_dummy = TRUE)
-dummy_cols <- function(data,
+dummy_cols <- function(.data,
                        select_columns = NULL,
                        remove_first_dummy = FALSE) {
 
@@ -38,55 +39,61 @@ dummy_cols <- function(data,
             select_columns != "",
             is.logical(remove_first_dummy), length(remove_first_dummy) == 1)
 
+  data_type <- check_type(.data)
 
-  if (!data.table::is.data.table(data)) {
-    data <- data.table::as.data.table(data)
+  if (!data.table::is.data.table(.data)) {
+    .data <- data.table::as.data.table(.data)
   }
 
   # Grabs column names that are character or factor class -------------------
   if (!is.null(select_columns)) {
     char_cols <- select_columns
-    cols_not_in_data <- char_cols[!char_cols %in% names(data)]
+    cols_not_in_data <- char_cols[!char_cols %in% names(.data)]
     char_cols <- char_cols[!char_cols %in% cols_not_in_data]
     if (length(char_cols) == 0) {
       stop("select_columns is/are not in data. Please check data and spelling.")
     }
-  } else if (ncol(data) == 1) {
-    char_cols <- names(data)
+  } else if (ncol(.data) == 1) {
+    char_cols <- names(.data)
   } else {
-    char_cols <- sapply(data, class)
+    char_cols <- sapply(.data, class)
     char_cols <- char_cols[char_cols %in% c("factor", "character")]
     char_cols <- names(char_cols)
   }
 
   if (length(char_cols) == 0 && is.null(select_columns)) {
-    stop("No character or factor columns found. Please use select_columns to choose columns.")
+    stop(paste0("No character or factor columns found. ",
+                "Please use select_columns to choose columns."))
   }
 
   if (!is.null(select_columns) && length(cols_not_in_data) > 0) {
-    warning("NOTE: The following select_columns input(s) is not a column in data.\n", paste0(names(cols_not_in_data), "\t"))
+    warning(paste0("NOTE: The following select_columns input(s) ",
+                   "is not a column in data.\n"),
+                   paste0(names(cols_not_in_data), "\t"))
   }
 
 
   for (col_name in char_cols) {
-    unique_vals <- as.character(unique(data[[col_name]]))
+    unique_vals <- as.character(unique(.data[[col_name]]))
 
     if (remove_first_dummy) {
       unique_vals <- unique_vals[-1]
     }
-
-    data.table::set(data, j = paste0(col_name, "_", unique_vals), value = 0L)
+    data.table::alloc.col(.data, ncol(.data) + length(unique_vals))
+    data.table::set(.data, j = paste0(col_name, "_", unique_vals), value = 0L)
     for (unique_value in unique_vals) {
-      data.table::set(data, i = which(as.character(data[[col_name]])
-                                      %in% unique_value),
-                      j = paste0(col_name, "_", unique_value), value = 1L)
-    }
+       data.table::set(.data, i = which(data.table::chmatch(as.character(.data[[col_name]]),
+                                                            unique_value) == 1L),
+                       j = paste0(col_name, "_", unique_value), value = 1L)
+
+     }
   }
 
-  data <- as.data.frame(data, stringsAsFactors = FALSE)
-  return(data)
+  .data <- fix_data_type(.data, data_type)
+  return(.data)
 
 }
+
 
 #' Fast creation of dummy variables
 #'
--- R/dummy_rows.R
+++ R/dummy_rows.R
@@ -10,21 +10,22 @@
 #' @family dummy functions
 #' @seealso \code{\link{dummy_cols}} For creating dummy columns
 #'
-#' @param data
+#' @param .data
 #' An object with the data set you want to make dummy columns from.
 #' @param select_columns
 #' If NULL (default), uses all character, factor, and Date columns to produce categories
 #' to make the dummy rows by. If not NULL, you manually enter a string or vector of strings of columns name(s).
 #' @param dummy_value
 #' Value of the row for columns that are not selected.
-#' Default is a value of 0.
+#' Default is a value of NA.
 #' @param dummy_indicator
 #' Adds binary column to say if row is dummy or not (i.e. included in
 #' original data or not)
 #'
 #' @return
-#' A data.frame with same number of columns as inputted data and original
-#' rows plus the newly created dummy rows
+#' A data.frame (or tibble or data.table, depending on input data type) with
+#' same number of columns as inputted data and original rows plus the newly
+#' created dummy rows
 #' @export
 #' @examples
 #' crime <- data.frame(city = c("SF", "SF", "NYC"),
@@ -40,7 +41,7 @@
 #' # Add a dummy indicator
 #' dummy_rows(crime, select_columns = c("city", "year"),
 #'     dummy_indicator = TRUE)
-dummy_rows <- function(data,
+dummy_rows <- function(.data,
                        select_columns = NULL,
                        dummy_value = NA,
                        dummy_indicator = FALSE) {
@@ -50,18 +51,20 @@ dummy_rows <- function(data,
             is.logical(dummy_indicator), length(dummy_indicator) == 1,
             length(dummy_value) == 1)
 
-  if (is.atomic(data) || ncol(data) == 1) {
+  if (is.atomic(.data) || ncol(.data) == 1) {
     stop("Cannot make dummy rows of a vector of one column data.frame/table.")
   }
 
-  if (!data.table::is.data.table(data)) {
-    data <- data.table::as.data.table(data)
+  data_type <- check_type(.data)
+
+  if (!data.table::is.data.table(.data)) {
+    .data <- data.table::as.data.table(.data)
   }
 
   # Finds class of every column and keeps character, factor, and Date --------
   if (is.null(select_columns)) {
-    char_cols <- sapply(data, class)
-    char_cols <- names(data)[char_cols %in%
+    char_cols <- sapply(.data, class)
+    char_cols <- names(.data)[char_cols %in%
                                c("character", "factor", "Date")]
     if (length(char_cols) == 0) {
       stop("No character, factor, or Date columns found.")
@@ -71,41 +74,45 @@ dummy_rows <- function(data,
     char_cols <- select_columns
   }
 
-  other_cols <- names(data)[!names(data) %in% char_cols]
+  other_cols <- names(.data)[!names(.data) %in% char_cols]
 
 
   # Finds how many possible combinations of the variables there are.
   # This will be the number of rows in the new data
- # return(data[, char_cols, with = FALSE])
-  total_length <- prod(sapply(data[, char_cols, with = FALSE, drop = FALSE],
+  total_length <- prod(sapply(.data[, char_cols, with = FALSE, drop = FALSE],
                               data.table::uniqueN))
   # Makes an empty data.table with right # of rows and columns. -------------
   temp_table <- data.table::data.table(matrix(nrow = total_length,
-                                              ncol = ncol(data)))
-  names(temp_table) <- names(data)
+                                              ncol = ncol(.data)))
+  names(temp_table) <- names(.data)
 
   # Fills in all possible combination rows ----------------------------------
     for (i in char_cols) {
         data.table::set(temp_table, j = i,
-                      value = rep(unique(data[[i]]), times =
+                      value = rep(unique(.data[[i]]), times =
                                   total_length /
-                                  data.table::uniqueN(data[[i]])))
+                                  data.table::uniqueN(.data[[i]])))
      temp_table <- data.table::setorderv(temp_table, i)
     }
 
   # Adds the dummy variable columns (and indicator) -------------------------
   for (i in other_cols) {
-    data.table::set(temp_table, j = other_cols, value = rep(dummy_value, nrow(temp_table)))
+    data.table::set(temp_table, j = other_cols,
+                    value = rep(dummy_value, nrow(temp_table)))
   }
 
   if (dummy_indicator) {
-    data.table::alloc.col(temp_table, ncol(temp_table) + 1) # Adding extra column
-    data.table::set(data, j = "dummy_indicator", value = 0L)
-    data.table::set(temp_table, j = "dummy_indicator", value = rep(1L, nrow(temp_table)))
+    # Adding extra column
+    data.table::alloc.col(temp_table, ncol(temp_table) + 1)
+    data.table::alloc.col(.data, ncol(.data) + 1)
+
+    data.table::set(.data, j = "dummy_indicator", value = 0L)
+    data.table::set(temp_table, j = "dummy_indicator",
+                    value = rep(1L, nrow(temp_table)))
   }
 
   # Removes rows that were in original data. --------------------------------
-  data_temp_pasting <- do.call(paste0, data[, char_cols, with = FALSE,
+  data_temp_pasting <- do.call(paste0, .data[, char_cols, with = FALSE,
                                             drop = FALSE])
   temp_temp_pasting <- do.call(paste0, temp_table[, char_cols, with = FALSE,
                                             drop = FALSE])
@@ -113,10 +120,11 @@ dummy_rows <- function(data,
 
   # Stacks new data on old data
   if (nrow(temp_table) > 0) {
-  data <- data.table::rbindlist(list(data, temp_table), use.names = TRUE,
+  .data <- data.table::rbindlist(list(.data, temp_table), use.names = TRUE,
                                 fill = TRUE)
   }
-  data <- as.data.frame(data)
-  return(data)
+
+  .data <- fix_data_type(.data, data_type)
+  return(.data)
 
 }
