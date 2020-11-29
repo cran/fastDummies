98a5317851562a679a8f6e634517cbe3c4b9bdd0
Jacob Kaplan <jkkaplan6@gmail.com>
2020-11-29 15:30:03 +0000

    version 1.6.3

--- tests/testthat/test-columns-value-order.R
+++ tests/testthat/test-columns-value-order.R
@@ -10,6 +10,16 @@ numeric_order <- data.frame(photos = c(1, 5, 7, 2, 40, 23, 12, 6, 1))
 
 test_that("Order of dummy columns (e.g. 0,0,1,0) is right", {
 
+
+  expect_equal(dummy_cols(1:3)$.data_1, c(1, 0, 0))
+  expect_equal(dummy_cols(1:3)$.data_2, c(0, 1, 0))
+  expect_equal(dummy_cols(1:3)$.data_3, c(0, 0, 1))
+
+  expect_equal(dummy_cols(c("a", "b", "c"))$.data_a, c(1, 0, 0))
+  expect_equal(dummy_cols(c("a", "b", "c"))$.data_b, c(0, 1, 0))
+  expect_equal(dummy_cols(c("a", "b", "c"))$.data_c, c(0, 0, 1))
+
+  expect_equal(dummy_cols(fastDummies_example)$gender_female, c(0, 0, 1))
   expect_equal(dummy_cols(fastDummies_example)$gender_male, c(1, 1, 0))
   expect_equal(dummy_cols(fastDummies_example)$gender_female, c(0, 0, 1))
   expect_equal(dummy_cols(fastDummies_example,
